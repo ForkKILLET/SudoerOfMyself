f1fbe8e68f9eb3499679cbfc2704346ddfd3ba94
ForkΨKILLET <fork_killet@qq.com>
2022-02-26 21:37:39 +0800

    feat(command.human.echo): Remove redundant `-` in short options.

--- src/command.js
+++ src/command.js
@@ -12,10 +12,10 @@ const humanPages = {
 	],
 	"echo": [
 		"显示一行思想。",
-		"`echo --angrily | --a`		生气地想",
-		"`echo --tremulously | --t`	发抖地想",
-		"`echo --seriously | --S`	严肃地想",
-		"`echo --sadly | --s`		伤心地想"
+		"`echo --angrily | -a`		生气地想",
+		"`echo --tremulously | -t`	发抖地想",
+		"`echo --seriously | -S`	严肃地想",
+		"`echo --sadly | -s`		伤心地想"
 	]
 }
 
